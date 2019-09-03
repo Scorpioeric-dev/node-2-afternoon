ed1bbd927095363e0f2f170e9f542072cd7e80dd
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-09-03 11:09:02 -0600

    node-2

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,48 +1,41 @@
-let messages = []
-let id = 0
-
+let messages = [];
+let id = 0;
 
 module.exports = {
-    
-    create: (req, res) => {
-        const {text, time} = req.body 
-        messages.push( {id,text,time} )
-        id++
-        req.status(200).send(messages)
-    },
-   
-    read: (req, res) => {
-        res.status(200).send(messages)
-    },
-    
-    upDate: (req, res) => {
-        const { text } = req.body
-        const updateId = req.params.id 
-        const messageIndex = messages.findIndex(message => message.id == updateId)
-        let message = messages[messageIndex]
+  create: (req, res) => {
+    //destructures text and time
+    const { text, time } = req.body;
+    messages.push({ id, text, time });
+    id++;
+    res.status(200).send(messages);
+  },
+  //returns the messages array
+  read: (req, res) => {
+    res.status(200).send(messages);
+  },
+  //this updates the text property using req.body and id using params(look at syntax)
+  upDate: (req, res) => {
+    const { text } = req.body;
+    const updateId = req.params.id;
+    const messageIndex = messages.findIndex(message => message.id == updateId);
+    let message = messages[messageIndex];
 
-        messages[messageIndex] = {
-            id: message.id,
-            text: text || message.text,
-            time: message.time
-        }
+    messages[messageIndex] = {
+      id: message.id,
+      text: text || message.text,
+      time: message.time
+    };
 
-        res.status(200).send(messages)
-    },
-   
-    Delete: (req, res ) => {
-       const deleteId = req.params.id
-       const messageIndex = messages.findIndex(message => message.id == deleteId)
-       messages.splice(messageIndex, 1)       
-       
-       
-        res.status(200).send(messages)
-    }
-}
+    res.status(200).send(messages);
+  },
+  //deletes messages according to id
+  delete: (req, res) => {
+    const {id} = req.params;
+    const index = messages.findIndex(message => message.id === +id);
+    messages.splice(index, 1);
+    res.status(200).send(messages);
+  }
 
+};
 
-
-
-
-
-
+  
--- server/index.js
+++ server/index.js
@@ -3,28 +3,19 @@ const app = express()
 const port = 3001
 const messagesCtrl = require('./controllers/messages_controller')
 
-
+//middleware
 app.use(express.json())
-
 app.use(express.static(__dirname + '/../public/build'))
 
-
-app.post(`/api/messages`)
-
-
-
-
-app.get(`/api/messages`)
-
-
-
+//endpoints
+app.post(`/api/messages`,messagesCtrl.create)
+app.get(`/api/messages`,messagesCtrl.read)
+//params,body,query backend points (id)
 app.put(`/api/messages/:id`, messagesCtrl.upDate)
-
-
-app.delete(`api/messages/:id`, messagesCtrl.Delete)
+app.delete(`/api/messages/:id`, messagesCtrl.delete)
 
 
 app.listen(port, () => {
-    console.log (`Run DMC $[port]`)
+    console.log(`Run DMC ${port}`)
 })
 
